ddae2f04dd92ea1835c7fc2799c3127189fb5d7c
xh1996819 <437336107@qq.com>
2019-12-16 00:40:46 +0800

    多选组件练习

---
+++ src/components/checkList/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <el-checkbox-group v-model="checkList">
+      <el-checkbox v-for="(item, i) in chooseGroup" :key="i" :label="item.name"></el-checkbox>
+    </el-checkbox-group>
+
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="sendCheckList">确 定</el-button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 1,
+      checkList: [],
+      chooseGroup: [],
+      sendList: []
+    };
+  },
+  created() {},
+  mounted() {
+    this.getCheckList();
+  },
+  methods: {
+    getCheckList() {
+      setTimeout(() => {
+        for (let index = 0; index < 8; index++) {
+          var obj = {
+            name: `${this.number}号选手`
+          };
+          this.chooseGroup = [...this.chooseGroup, obj];
+
+          this.number++;
+          //   console.log(obj);
+
+          //   console.log(this.chooseGroup);
+        }
+      }, 150);
+    },
+    sendCheckList() {
+      //   console.log(this.checkList);
+      this.sendList.filter(item => !this.checkList.includes(item));
+      this.$emit("getCheckList", this.sendList);
+    }
+  }
+  //   watch: {
+  //     checkList(nV, oV) {
+  //       //   console.log(nV, oV);
+  //       let array = [];
+  //       array = nV.filter(item => !oV.includes(item));
+  //       console.log(array);
+  //       this.sendList = array
+  //     }
+  //   }
+};
+</script>
--- src/views/setting/holiday_admin/index.vue
+++ src/views/setting/holiday_admin/index.vue
@@ -1,5 +1,48 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <el-button type="text" @click="dialogVisible = true">点击选择选手</el-button>
+    <el-dialog title="选择选手" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+      <check-list @getCheckList="getCheckList"></check-list>
+    </el-dialog>
+
+    <el-table :data="tableData" border style="width: 50%">
+      <el-table-column prop="date" label="添加时间"></el-table-column>
+      <el-table-column prop="name" label="选手"></el-table-column>
+    </el-table>
+  </div>
 </template>
+
+<script>
+import moment from "moment";
+import checkList from "@/components/checkList";
+export default {
+  data() {
+    return {
+      dialogVisible: false,
+      tableData: []
+    };
+  },
+  components: { checkList },
+  methods: {
+    handleClose(done) {
+      done();
+    },
+    getCheckList(checkList) {
+      //   console.log(checkList);
+      checkList.map((item, i) => {
+        this.tableData.push(
+          Object.assign(
+            {},
+            { name: item },
+            {
+              date: moment(new Date()).format("YYYY-MM-DD hh:mm:ss")
+            }
+          )
+        );
+      });
+      //   this.tableData.filter(item => !checkList.includes(item.name));
+      console.log(this.tableData);
+    }
+  }
+};
+</script>
