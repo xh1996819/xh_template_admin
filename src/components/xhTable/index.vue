8c5143f85d6fc9e9a0c30f4756bff03b367e88cb
xh1996819 <437336107@qq.com>
2019-12-18 23:26:51 +0800

    新增xhTable组件

---
+++ src/components/xhTable/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column
+      v-for="item in tableData.length + 1"
+      :key="item"
+      :prop="propList[item - 1]"
+      :label="title[item - 1]"
+      width="180"
+    ></el-table-column>
+    <!-- <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+    <el-table-column prop="address" label="地址"></el-table-column>-->
+  </el-table>
+</template>
+
+<script>
+export default {
+  props: {
+    tableData: {
+      type: Array,
+      default: []
+    },
+    title: {
+      type: Array,
+      default: []
+    }
+  },
+  data() {
+    return {
+      propList: []
+    };
+  },
+  created() {
+    this.tableData.forEach(item => {
+      this.propList = Object.keys(item);
+    });
+    console.log(this.propList);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/classManage/index.vue
+++ src/views/classManage/index.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        123
-        <el-button @click="$router.push({name:'ClassManageControl'})"></el-button>
-    </div>
+  <div>
+    <el-button @click="$router.push({name:'ClassManageControl'})"></el-button>
+    <xh-table :tableData="classList" :title="title"></xh-table>
+  </div>
 </template>
+
+<script>
+import xhTable from "@/components/xhTable";
+export default {
+  data() {
+    return {
+      classList: [
+        { name: "张三", time: "2019/9/19", id: "1" },
+        { name: "李四", time: "2019/10/20", id: "2" }
+      ],
+      title: ["姓名", "时间", "id"]
+    };
+  },
+  components: { xhTable }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
