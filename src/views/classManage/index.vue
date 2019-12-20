566d3dadfba4ab58950c8e18ac0a8f63c6a6b714
theBestXH <437336107@qq.com>
2019-12-20 19:06:39 +0800

    验证码功能摸索

--- src/views/classManage/index.vue
+++ src/views/classManage/index.vue
@@ -2,6 +2,16 @@
   <div>
     <el-button @click="$router.push({name:'ClassManageControl'})"></el-button>
     <xh-table :tableData="classList" :title="title"></xh-table>
+    <div class="input-box">
+      <input
+        v-for="item in 4"
+        :key="item"
+        :ref="`input${item}`"
+        maxlength="1"
+        type="text"
+        @keyup="nextFocus(item)"
+      />
+    </div>
   </div>
 </template>
 
@@ -21,9 +31,26 @@ export default {
   created() {
     let date = Date.parse(new Date());
     console.log(this.$date.dateFormat(date, "Y/m/d H:i:s"));
+  },
+  mounted() {},
+  methods: {
+    nextFocus(item) {
+      if (item <= 3) {
+        this.$refs[`input${item + 1}`][0].focus();
+      }
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.input-box {
+  display: flex;
+  input {
+    width: 50px;
+    height: 50px;
+    border: 1px solid #000;
+    text-align: center;
+  }
+}
 </style>
